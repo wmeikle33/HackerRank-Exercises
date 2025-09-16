07596b557a8d14121d643d644e4b70595a50f8ee
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2025-09-16 10:46:27 +0800

    Create Extra Long Factorials.py

---
+++ src/Extra Long Factorials.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def extraLongFactorials(n):
+    print(math.factorial(n))
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    extraLongFactorials(n)
